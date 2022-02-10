59f934f4dae10875a55614f25e5e0f4cf713772d
志佳 <495725428@qq.com>
2022-02-10 18:53:16 +0800

    fix commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> 测试用例
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
